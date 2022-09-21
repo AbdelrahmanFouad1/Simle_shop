ee301e90425ca2fe3a66233f8edf2662d2c8fd48
Abdullah Mansour <abdofouad.cs@gmail.com>
2022-09-21 09:12:45 +0200

    161

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const { Model } = require('sequelize');
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const user = sequelize.define('user', {
+    id : {
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        allowNull : false,
+        primaryKey: true
+    },
+    name : sequelize.STRING,
+    email : sequelize.STRING
+});
+
+Model.exports = user;
