2a01b2218ea728e48a1de8c43d058223a529a93b
Abdelrahman Fouad <abdofouad.cs@gmail.com>
2023-01-14 15:15:28 +0300

    165

--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('first_node', 'root', 'Aa01555156821', {
+const sequelize = new Sequelize('node-complete', 'root', 'Aa01555156821', {
     dialect: 'mysql',
     host: 'localhost'
 });
+
 
 module.exports = sequelize;
 
